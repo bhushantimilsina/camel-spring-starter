134e66ddffe5d9931ebe0ae18a9eb3420762f2d4
bhushantimilsina <bhushantimilsina@gmail.com>
2021-03-05 10:36:49 -0500

    Initial commit

--- runMain.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-echo --------------------------------------
-echo    SIMPLE CAMEL APP
-echo --------------------------------------
-echo.
-echo Starting ...
-call mvn -q compile exec:java -Dexec.mainClass="org.bhushan.test.App"
-echo.
