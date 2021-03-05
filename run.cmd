2483fe13567ccb5f9b6fc6dc20fdeb387724a2f8
Ranjanabanjara <54809999+Ranjanabanjara@users.noreply.github.com>
2021-03-04 20:14:58 -0500

    Added run script

---
+++ run.cmd
@@ -0,0 +1,7 @@
+@echo off
+echo --------------------------------------
+echo    SIMPLE CAMEL APP
+echo --------------------------------------
+echo.
+mvn compile exec:java -Dexec.mainClass="org.bhushan.test.App"
+echo.
